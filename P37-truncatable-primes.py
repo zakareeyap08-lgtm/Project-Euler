8179434124e0c4b26a0b09ac54b07b947e94179f
zakareeyap08-lgtm <zakareeyap08@gmail.com>
2026-09-24 09:36:05 +0100

    Create P37-truncatable-primes.py

---
+++ P37-truncatable-primes.py
@@ -0,0 +1,41 @@
+import math
+
+truncatables = []
+num = 11
+sum = 0
+
+while len(truncatables) != 11:
+    isprime = True
+    num += 2
+    temp = str(num)
+    for i in range(2,int(math.sqrt(num)+1)):
+        if num % i == 0:
+            isprime = False
+    if isprime:
+        for i in range(0,len(temp)-1):
+            temp = temp[:-1]
+            if int(temp) == 1 or int(temp) % 2 == 0:
+                isprime = False
+                break
+            for i in range(3,int(math.sqrt(int(temp))+1),2):
+                if int(temp) % i == 0:
+                    isprime = False
+                    break
+    if isprime:
+        temp = str(num)
+        for i in range(0,len(temp)-1):
+            temp = temp[1:]
+            if int(temp) == 1 or int(temp) % 2 == 0:
+                isprime = False
+                break
+            for i in range(3,int(math.sqrt(int(temp))+1),2):
+                if int(temp) % i == 0:
+                    isprime = False
+                    break
+    if isprime:
+        truncatables.append(num)
+        
+for i in range(0,len(truncatables)):
+    sum += truncatables[i]
+    
+print(sum)
